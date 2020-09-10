9ffd5e79a849cb912c0be9eb9738da0de1984dc2
Marcos Lobos <Mdt19290@gmail.com>
2020-09-09 20:33:45 -0400

    Completed both java classes, and both have been tested by me at least

--- Lobos_p1/src/Decrypter.java
+++ Lobos_p1/src/Decrypter.java
@@ -1,7 +1,49 @@
-import java.util.Scanner;
-
 public class Decrypter {
-    public static void main(String[] args) {
+int firstDigit;
+int secondDigit;
+int thirdDigit;
+int fourthDigit;
 
+String firstChar;
+String secondChar;
+String thirdChar;
+String fourthChar;
+
+    public String decrypt(String ciphertext){
+        String plainttext;
+        digitAssign(ciphertext);
+
+        this.firstDigit = (this.firstDigit + 3) % 10;
+        this.secondDigit = (this.secondDigit + 3) % 10;
+        this.thirdDigit = (this.thirdDigit + 3) % 10;
+        this.fourthDigit = (this.fourthDigit + 3) % 10;
+
+        charAssign();
+
+        plainttext = compose();
+
+        return plainttext;
+    }
+
+    public void digitAssign(String plaintext){
+        this.firstDigit = plaintext.charAt(0) - 48;
+        this.secondDigit = plaintext.charAt(1) - 48;
+        this.thirdDigit = plaintext.charAt(2) - 48;
+        this.fourthDigit = plaintext.charAt(3) - 48;
+    }
+
+    public void charAssign(){
+        firstChar = Integer.toString(this.firstDigit);
+        secondChar = Integer.toString(this.secondDigit);
+        thirdChar = Integer.toString(this.thirdDigit);
+        fourthChar = Integer.toString(this.fourthDigit);
+    }
+
+    public String compose(){
+        String composedMsg;
+
+        composedMsg = (this.thirdChar + this.fourthChar + this.firstChar + this.secondChar);
+
+        return composedMsg;
     }
 }
--- Lobos_p1/src/Encrypter.java
+++ Lobos_p1/src/Encrypter.java
@@ -1,7 +1,50 @@
-import java.util.Scanner;
-
 public class Encrypter {
-    public static void main(String[] args) {
+int firstDigit;
+int secondDigit;
+int thirdDigit;
+int fourthDigit;
 
+String firstChar;
+String secondChar;
+String thirdChar;
+String fourthChar;
+
+    public String encrypt(String plaintext){
+        String ciphertext;
+        digitAssign(plaintext);
+
+        this.firstDigit = (this.firstDigit + 7) % 10;
+        this.secondDigit = (this.secondDigit + 7) % 10;
+        this.thirdDigit = (this.thirdDigit + 7) % 10;
+        this.fourthDigit = (this.fourthDigit + 7) % 10;
+
+        charAssign();
+
+        ciphertext = compose();
+
+        return ciphertext;
     }
+
+    public void digitAssign(String plaintext){
+        this.firstDigit = plaintext.charAt(0) - 48;
+        this.secondDigit = plaintext.charAt(1) - 48;
+        this.thirdDigit = plaintext.charAt(2) - 48;
+        this.fourthDigit = plaintext.charAt(3) - 48;
+    }
+
+    public void charAssign(){
+        firstChar = Integer.toString(this.firstDigit);
+        secondChar = Integer.toString(this.secondDigit);
+        thirdChar = Integer.toString(this.thirdDigit);
+        fourthChar = Integer.toString(this.fourthDigit);
+    }
+
+    public String compose(){
+        String composedMsg;
+
+        composedMsg = (this.thirdChar + this.fourthChar + this.firstChar + this.secondChar);
+
+        return composedMsg;
+    }
+
 }
